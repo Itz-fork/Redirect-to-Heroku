c5d1fd477714be2a508b26dc37fd74ee1ed445be
Itz-fork <git.itzfork@gmail.com>
2022-02-06 19:38:02 +0530

    Initial Release

---
+++ README.md
@@ -0,0 +1,18 @@
+# Redirect to Heroku
+This is a simple website to get the heroku deploy link from a github repository.
+
+## Usage
+
+- Open the [Redirect to Heroku Website](https://itz-fork.github.io/Redirect-to-Heroku)
+    - You can pass `src` parameter to add fork button on the page. Once you passed it you can't get the deploy link for the same github repo
+    - **Ex:** `https://itz-fork.github.io/Redirect-to-Heroku?src=YOUR-GITHUB-USERNAME/REPO-NAME`
+
+- Enter your **forked github repo link** in the input field
+- Click on the "Deploy Now!" button
+
+That's it!
+
+## License & Credits
+
+- This repo is licensed under [MIT LICENSE](https://github.com/Itz-fork/Redirect-to-Heroku/blob/main/LICENSE)
+- Main image credits goes to [Dyepell](https://www.deviantart.com/dyepell/art/Sagiri-676662189)
---
+++ app.js
@@ -0,0 +1,57 @@
+// Function to validate src parameter
+function validateSrc() {
+    if (!unr) {
+        unr = "Itz-fork";
+        return throwError(true, "'src' parameter not found! Some functions may won't work!");
+    }
+    let spl = unr.split("/");
+    if (spl.length < 2 || spl[1] == "") {
+        return throwError();
+    }
+}
+
+// Function to setup fork button
+function setupForkButton() {
+    if (unr == "Itz-fork") {
+        document.getElementById("fork").innerHTML = `<a class="github-button" href="https://github.com/Itz-fork" data-color-scheme="no-preference: dark_dimmed; light: dark_dimmed; dark: dark_dimmed;" data-size='large' aria-label="Follow @Itz-fork on GitHub">Follow @Itz-fork</a>`
+    } else {
+        document.getElementById("fork").innerHTML = `<a class='github-button' href='https://github.com/${unr}' data-color-scheme='no-preference: dark_high_contrast; light: dark_dimmed; dark: dark_dimmed;' data-icon='octicon-repo-forked' data-size='large' data-show-count='true' aria-label='Fork ${unr} on GitHub'>Fork it!</a>`
+    }
+}
+
+
+// Function to redirect to heroku
+function redirectToHeroku() {
+    let input = document.getElementById("repo-link").value;
+    if (!input || input == "") {
+        return alert("Please enter your forked repo link!");
+    }
+
+    if (input.includes(unr)) {
+        return alert("You are trying to deploy the original repo which was banned from heroku. Please enter your forked repo link or kindly fuck off!");
+    }
+
+    const regex = /https?:\/\/github\.com\/(?:[^\/\s]+\/)+/g
+    if (!regex.test(input)) {
+        return alert("Please enter a github repo url!");
+    }
+
+    window.open(`https://heroku.com/deploy?template=${input}`, "_blank")
+}
+
+// Function to throw errors
+function throwError(asAlert = false, alTxt = null) {
+    if (asAlert) {
+        alert(alTxt);
+    } else {
+        window.location = "oops.html";
+    }
+}
+
+
+// Get the url parameters
+let req = new URL(window.location.href);
+var unr = req.searchParams.get("src");
+
+validateSrc(unr)
+setupForkButton()
