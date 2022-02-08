e58e14c84a54c18ca83629d31e7e687546114fa1
Itz-fork <git.itzfork@gmail.com>
2022-02-08 22:18:36 +0530

    Removed some unnecessary codes

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ function validateSrc() {
 // Function to setup fork button
 function setupForkButton() {
     if (unr == "Itz-fork") {
-        document.getElementById("fork").innerHTML = `<a class="github-button" href="https://github.com/Itz-fork" data-color-scheme="no-preference: dark_dimmed; light: dark_dimmed; dark: dark_dimmed;" data-size='large' aria-label="Follow @Itz-fork on GitHub">Follow @Itz-fork</a>`
+        document.getElementById("fork").innerHTML = `<a class="github-button" href="https://github.com/Itz-fork" data-color-scheme="no-preference: dark_dimmed; light: dark_dimmed; dark: dark_dimmed;" data-size='large' aria-label="Follow @Itz-fork on GitHub">Follow @Itz-fork</a>`;
     } else {
-        document.getElementById("fork").innerHTML = `<a class='github-button' href='https://github.com/${unr}' data-color-scheme='no-preference: dark_high_contrast; light: dark_dimmed; dark: dark_dimmed;' data-icon='octicon-repo-forked' data-size='large' data-show-count='true' aria-label='Fork ${unr} on GitHub'>Fork it!</a>`
+        document.getElementById("fork").innerHTML = `<a class='github-button' href='https://github.com/${unr}' data-color-scheme='no-preference: dark_high_contrast; light: dark_dimmed; dark: dark_dimmed;' data-icon='octicon-repo-forked' data-size='large' data-show-count='true' aria-label='Fork ${unr} on GitHub'>Fork it!</a>`;
     }
 }
 
@@ -31,17 +31,17 @@ function redirectToHeroku() {
         return throwError(true, "You are trying to deploy the original repo which was banned from heroku. Please enter your forked repo link or kindly fuck off!");
     }
 
-    const regex = /https?:\/\/github\.com\/(?:[^\/\s]+\/)+/g
+    const regex = /https?:\/\/github\.com\/(?:[^\/\s]+\/)+/g;
     if (!regex.test(input)) {
         return throwError(true, "Please enter a github repo url!");
     }
 
-    window.open(`https://heroku.com/deploy?template=${input}`, "_blank")
+    window.open(`https://heroku.com/deploy?template=${input}`, "_blank");
 }
 
 // Function to throw errors
-function throwError(asAlert = false, alTxt = null) {
-    if (asAlert) {
+function throwError(alTxt = null) {
+    if (alTxt) {
         alert(alTxt);
     } else {
         window.location = "oops.html";
@@ -50,8 +50,7 @@ function throwError(asAlert = false, alTxt = null) {
 
 
 // Get the url parameters
-let req = new URL(window.location.href);
-var unr = req.searchParams.get("src");
+var unr = new URL(window.location.href).searchParams.get("src");
 
-validateSrc(unr)
-setupForkButton()
+validateSrc();
+setupForkButton();
